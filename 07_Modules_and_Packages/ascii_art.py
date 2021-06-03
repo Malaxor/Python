a2031030abca0f1f346af86701e43a7f3b6def1b
Malaxor <nogobaduk@gmail.com>
2021-06-03 15:23:10 -0400

    http requests mini-project

--- 07_Modules_and_Packages/ascii_art.py
+++ 07_Modules_and_Packages/ascii_art.py
@@ -2,17 +2,33 @@ from colorama import init
 from termcolor import colored
 from pyfiglet import figlet_format as format
 
-init()
-colors = ('red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
+def color_text():
+    colors = ('red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
+    msg = ''
+    color = 'black'
+    
+    while not (msg and color in colors):
+        msg = input("What woud you like to print? ")
+        color = input("which color? ")
+    
+    return colored(format(msg), color = color)
 
-msg = input("what would you like to print? ")
-if not msg:
-   msg = 'Spring'
 
-color = input("what color? ") 
-
-try: 
-   result = colored(format(msg), color = color)
-   print(result)
-except KeyError:
-   print(colored(format(msg), color = 'green'))
+def color_text2():
+    msg = ''
+    
+    while not msg:
+        msg = input("What would you like to print? ")
+        
+    while True:
+        color = input("Which color? ")
+        
+        try:
+            colored(format(msg), color = color)
+        except KeyError:
+            print('Color unavailable.')
+        else:
+            break
+            
+    print(colored(format(msg), color = color))
+   
