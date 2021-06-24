86200be90d119731e34f2df61d07aa71b05c4144
Malaxor <nogobaduk@gmail.com>
2021-06-24 15:48:54 -0400

    SQL-lite: creating tables and inserting data

---
+++ 23_Python_and_SQL/tables.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE dogs (
+--    name TEXT,
+--    breed TEXT,
+--    age INTEGER
+-- );
+
+-- CREATE TABLE cats (
+--    name TEXT,
+--    breed TEXT,
+--    age INTEGER
+-- )
+
+INSERT INTO dogs(name, breed, age) 
+VALUES
+('Champ', 'Husky', 4),
+('Rose', 'Chihuahua', 11),
+('Moose', 'Lab', 5),
+('Mumu', 'Lab', 7);
