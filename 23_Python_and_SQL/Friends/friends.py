1996a9d4729f3b29dd8384e530d34f189d815be6
Malaxor <nogobaduk@gmail.com>
2021-06-24 16:53:35 -0400

    sql: bulk inserting

--- 23_Python_and_SQL/Friends/friends.py
+++ 23_Python_and_SQL/Friends/friends.py
@@ -4,14 +4,15 @@ conn = sqlite3.connect('my_friends.db')
 # create cursor object
 c = conn.cursor()
 # execute some SQL
-# c.execute("CREATE TABLE friends(first_name TEXT, last_name TEXT, closeness INTEGER)")
-# insert_query = """
-#    INSERT INTO friends
-#    VALUES('Charlie', 'Lewis', 7)
-# """
-data = ("Steve", "Irwin", 9)
+people = [
+   ("Roald", "Amundsen", 5),
+   ("Rosa", "Parks", 8),
+   ("Henry", "Hudson", 7),
+   ("Neil", "Armstrong", 6),
+   ("Daniel", "Boone", 3)
+]
 query = "INSERT INTO friends VALUES (?,?,?)"
-c.execute(query, data)
+c.executemany(query, people)
 # commit changes
 conn.commit()
 
