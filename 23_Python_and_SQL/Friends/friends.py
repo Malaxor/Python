620c531b2f41005afc31370f160b4132be916140
Malaxor <nogobaduk@gmail.com>
2021-06-24 16:47:47 -0400

    sql: inserting data with python

--- 23_Python_and_SQL/Friends/friends.py
+++ 23_Python_and_SQL/Friends/friends.py
@@ -4,7 +4,15 @@ conn = sqlite3.connect('my_friends.db')
 # create cursor object
 c = conn.cursor()
 # execute some SQL
-c.execute("CREATE TABLE friends(first_name TEXT, last_name TEXT, closeness INTEGER)")
+# c.execute("CREATE TABLE friends(first_name TEXT, last_name TEXT, closeness INTEGER)")
+# insert_query = """
+#    INSERT INTO friends
+#    VALUES('Charlie', 'Lewis', 7)
+# """
+data = ("Steve", "Irwin", 9)
+query = "INSERT INTO friends VALUES (?,?,?)"
+c.execute(query, data)
 # commit changes
 conn.commit()
+
 conn.close()
